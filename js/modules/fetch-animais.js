5a775cdbd0f983e03d5c82cba4a5ffc7c772f9ed
Luis Henrique <luishcardoso228@gmail.com>
2024-01-24 19:50:11 -0300

    fetchanimais refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,28 +1,37 @@
 import AnimaNumeros from './numeros.js';
 
-export default function initFetch() {
+export default function fetchAnimais(url, target) {
   function criaSection(json) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
     div.innerHTML = `<h3>${json.specie}</h3><span data-numero>${json.numeros}</span>`;
     return div;
   }
-  async function fetchAnimais() {
-    try {
-      const animaisResponse = await fetch(`${window.origin}/data.json`);
-      const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector('.numero-grid');
-      animaisJSON.forEach((animal) => {
-        const divAnimal = criaSection(animal);
+  const numerosGrid = document.querySelector(target);
+  function preencherAnimais(animal){
+    const divAnimal = criaSection(animal);
         numerosGrid.appendChild(divAnimal);
-      });
-      const animanumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
-      animanumeros.init();
+  }
 
+  function animaAnimaisNumeros(){
+    const animanumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+    animanumeros.init();
+
+  }
+  //Cria a div contendo informações através de um JSON 
+  //e cria um animal através do create animal
+  async function criarAnimais() {
+    try {
+      //fetch e espera resposta
+      const animaisResponse = await fetch(url);
+      //pega esponse e cria json
+      const animaisJSON = await animaisResponse.json();
+      animaisJSON.forEach(animal => preencherAnimais(animal));
+      animaAnimaisNumeros()
+     
     } catch (erro) {
       console.log(erro);
     }
   }
-
-  fetchAnimais('./data.json');
+  return criarAnimais()
 }
