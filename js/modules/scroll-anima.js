889c782a74f6d4056cf53ab0f2aef9a1e58cec86
Luis Henrique <luishcardoso228@gmail.com>
2024-01-24 23:44:27 -0300

    Animacao ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -2,25 +2,43 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectiontop = sectionTop - this.windowMetade < 0;
-      if (isSectiontop) section.classList.add('ativo');
-      else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  // pega a distancia de cada item em relação
+  // ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+  // verifica distancia em relação
+  // ao scroll do site
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
     if (this.sections.length) {
-      this.animaScroll();
-      window.addEventListener('scroll', this.animaScroll);
+      this.checkDistance();
+      this.getDistance();
+      window.addEventListener('scroll', this.checkDistance);
     }
     return this;
+  }
+
+  disable() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
