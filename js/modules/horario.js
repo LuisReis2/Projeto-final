37054cd5f1aca4af6dcbcb36b6f1af618b10bc32
Luis Henrique <luishcardoso228@gmail.com>
2024-01-25 02:32:04 -0300

    fucionamento refatorado

--- js/modules/horario.js
+++ js/modules/horario.js
@@ -1,17 +1,38 @@
-export default function initHour() {
-  const date = new Date();
-  const horaAgora = date.getHours();
-  const diaAgora = date.getDay();
-  const icon = document.querySelector('[data-funcionamento]');
-  const dias = icon.dataset.dias.split(',').map(Number);
-  const hora = icon.dataset.horario.split(',').map(Number);
-
-  const hasDia = dias.indexOf(diaAgora) !== -1;
-  const hasHour = horaAgora >= hora[0] && horaAgora < hora[1];
-
-  if (hasDia && hasHour) {
-    icon.classList.add('aberto');
-  } else {
-    icon.classList.remove('aberto');
+export default class Hour {
+  constructor(funcionamento, activeClass){
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
   }
+
+
+  dadosFuncionamento(){
+    this.dias = this.funcionamento.dataset.dias.split(',').map(Number);
+    this.hora = this.funcionamento.dataset.horario.split(',').map(Number);
+  }
+  
+dadosAgora(){
+  this.date = new Date();
+  this.horaAgora = this.date.getUTCHours() - 3;
+  this.diaAgora = this.date.getDay();
+}
+  isOpen(){
+    const hasDia = this.dias.indexOf(this.diaAgora) !== -1;
+    const hasHour = this.horaAgora >= this.hora[0] && this.horaAgora < this.hora[1];
+    return hasHour && hasDia;
+  }
+
+  ativaAberto(){
+    if(this.isOpen()){
+     this.funcionamento.classList.add( this.activeClass);
+    } 
+  }
+  init(){
+    if(this.funcionamento){
+      this.dadosAgora();
+      this.dadosFuncionamento()
+      this.ativaAberto()
+    }
+    return this;
+  }
+ 
 }
